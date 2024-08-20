4072139b7ecc532b4a3358383044d894bcce81d0
MaoJBN <mjbecerran@ufpso.edu.co>
2024-08-20 06:56:36 -0500

    El Jlist en la u

---
+++ UTest/src/main/java/com/mycompany/utest/UTest.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.utest;
+
+/**
+ *
+ * @author SCIS2-10
+ */
+public class UTest {
+
+    public static void main(String[] args) {
+        
+    }
+}
